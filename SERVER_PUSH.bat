124a51ae8c3c527295c15d2cc44b82a5393c7c4c
nimishsantosh107 <nimishsantosh107@icloud.com>
2021-06-07 17:43:00 +0530

    _1743

--- SERVER_PUSH.bat
+++ SERVER_PUSH.bat
@@ -1,6 +1,10 @@
 ECHO "PUSHING..."
 
-git status & git add . & git commit -m 'UPDATE' & git push origin master
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+echo %mydate%_%mytime%
+
+git status & git add . & git commit -m %mydate%_%mytime% & git push origin master
 
 ECHO "PUSH DONE"
 EXIT
